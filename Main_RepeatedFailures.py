08749d01d47393877d93130458302c9554fd65cd
Jayden Navarro <jaynavar@stanford.edu>
2019-05-16 13:25:10 -0700

    Raised node capacity

--- Main_RepeatedFailures.py
+++ Main_RepeatedFailures.py
@@ -15,10 +15,10 @@ def runRepeatedFailuresExperiment(numNodes, numIntervals, numTrials):
    # Failures every: 1 hour, 1 day, 1 week (in seconds)
    failureIntervals = [1 * MINUTE, 30 * MINUTE, 1 * HOUR, 1 * DAY]
    replicationFactor = 3
-   # set node parameters (1 Gb/s, 100 GB per node), in Mb's, assuming
+   # set node parameters (1 Gb/s, 1 TB per node), in Mb's, assuming
    # each peer can only dedicate 30% of capacity to node recovery
    nodeBandwidth = 1000
-   nodeCapacity = 8 * 100000
+   nodeCapacity = 8 * 1000000
    recoveryUtil = 0.3
 
    intervalData = []
