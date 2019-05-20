cefc0a4b07a3cbb38ad75436624c564847f2fd8d
Jayden Navarro <jaynavar@stanford.edu>
2019-05-20 10:19:03 -0700

    Added more scatter widths

--- Main_RepeatedFailures.py
+++ Main_RepeatedFailures.py
@@ -21,7 +21,7 @@ DAY = 24 * HOUR
 WEEK = 7 * DAY
 
 def runRepeatedFailuresExperiment(numNodes, numIntervals, numTrials):
-   scatterWidths = [10, 200]
+   scatterWidths = [10, 20, 100, 200]
    failureIntervals = [1 * MINUTE, 25 * MINUTE, 50 * MINUTE]
    replicationFactor = 3
    # set node parameters (1 Gb/s, 100 GB per node), in Mb's, assuming
